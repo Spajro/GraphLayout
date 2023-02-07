12cf47517954952e54bd95f342a00cab7bf93c2d
Spajro <rafal.spyra@onet.pl>
2023-02-07 02:03:29 +0100

    add antialiasing

--- display.rs
+++ display.rs
@@ -1,5 +1,6 @@
 use image::{Rgba, RgbaImage};
 use imageproc::drawing::*;
+use imageproc::pixelops::interpolate;
 use rusttype::{Font, Scale};
 use crate::force_driven_layout::State;
 use crate::math2d::{add, NormalizedVector, scale_position_x, scale_position_y};
@@ -33,7 +34,7 @@ pub fn draw(state: &mut State) {
                                        Rgba([206, 127, 223, 200]),
                                        state.positions.get(&vertex).unwrap().x,
                                        state.positions.get(&vertex).unwrap().y,
-                                       Scale { x: 30.0, y: 30.0 },
+                                       Scale { x: 50.0, y: 50.0 },
                                        font,
                                        vertex.label.as_str());
     });
@@ -53,10 +54,11 @@ pub fn draw(state: &mut State) {
 
         i += 1;
 
-        image = draw_line_segment(&image,
-                                  (state.positions.get(&edge.first).unwrap().x as f32, state.positions.get(&edge.first).unwrap().y as f32),
-                                  (state.positions.get(&edge.second).unwrap().x as f32, state.positions.get(&edge.second).unwrap().y as f32),
-                                  Rgba([60, 252, 91, 200]));
+        image = draw_antialiased_line_segment(&image,
+                                              (state.positions.get(&edge.first).unwrap().x, state.positions.get(&edge.first).unwrap().y),
+                                              (state.positions.get(&edge.second).unwrap().x, state.positions.get(&edge.second).unwrap().y),
+                                              Rgba([60, 252, 91, 200]),
+                                              interpolate);
     });
 
     let _ = image.save("result.png");
